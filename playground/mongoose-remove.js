a92a09fcced750418723388b241e4391310bc669
Afam <afamezechukwu@gmail.com>
2018-08-09 09:49:54 +0100

    Add DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,18 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+//
+
+Todo.findOneAndRemove({_id: '5b6bfb669e6143e34590b055'}).then((todo) => {
+  console.log(todo);
+});
+
+Todo.findByIdAndRemove('5b6bfb669e6143e34590b055').then((todo) => {
+  console.log(todo);
+});
